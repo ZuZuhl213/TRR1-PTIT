4e118c64863fb5de4a8b5a2e781df00cf8dd1624
ZuZuhl213 <HoangLH.B22VT205@stu.ptit.edu.vn>
2026-04-17 03:04:10 +0700

    TRR1_1010

---
+++ TRR1_1010.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a, b, c;
+	cin >> a >> b >> c;
+
+	bool hasPositiveRoot = false;
+
+	if (a == 0) {
+		if (b != 0) {
+			double x = -c / b;
+			hasPositiveRoot = (x > 0);
+		}
+	} else {
+		double delta = b * b - 4 * a * c;
+		if (delta >= 0) {
+			double sqrtDelta = sqrt(delta);
+			double x1 = (-b - sqrtDelta) / (2 * a);
+			double x2 = (-b + sqrtDelta) / (2 * a);
+			hasPositiveRoot = (x1 > 0 || x2 > 0);
+		}
+	}
+
+	cout << (hasPositiveRoot ? 1 : 0);
+	return 0;
+}
